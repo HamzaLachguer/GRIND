502bf620433e78f69f8e9f7d902a9b1b3da6373d
hamzalachguer <lachguerhmza@gmail.com>
2026-02-18 01:17:42 +0100

    Applying react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+
+import Home from "../src/pages/home/Home"
+import Contact from "../src/pages/contact/Contact"
+import Checkout from "../src/pages/checkout/Checkout"
+import Shop from "../src/pages/shop/Shop"
+
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        GRIND
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/shop" element={<Shop />} />
+    </Routes>
   )
 }
 
